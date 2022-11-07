d796a1904d40eda45e7d5a4e81fc66635d155b5f
TEISSIER Nathan <luigivampa12@protonmail.ch>
2022-11-07 18:07:03 +0100

    ntn

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { WeatherService } from '../services/weather.service';
-import { tap } from 'rxjs/operators';
 
 
 @Component({
@@ -11,6 +10,10 @@ import { tap } from 'rxjs/operators';
 export class HomePage implements OnInit {
 
   temperature: any;
+  date = new Date();
+  city: string;
+  icon: string;
+  finalIcon: string;
 
   constructor(private weatherService: WeatherService) { }
 
@@ -22,6 +25,9 @@ export class HomePage implements OnInit {
     this.weatherService.getWeather()
         .subscribe( (data: any) => {
           this.temperature = data.main;
+          this.city = data.name;
+          this.icon = data.weather[0].icon;
+          this.finalIcon = `http://openweathermap.org/img/wn/${this.icon}@4x.png`;
         });
   }
 
